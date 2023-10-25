b15df088090d9d90f0d61f7b812c4b294c6d3dbb
William Hua <WilliamHua2@my.unt.edu>
2023-10-25 14:01:42 -0400

    linked main with device manager, started implementing device connectivity features

--- app/src/main/java/com/example/rad/DeviceManagerActivity.java
+++ app/src/main/java/com/example/rad/DeviceManagerActivity.java
@@ -1,6 +1,93 @@
 package com.example.rad;
 
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import android.content.Intent;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class DeviceManagerActivity extends AppCompatActivity {
+public class DeviceManagerActivity extends AppCompatActivity implements View.OnClickListener {
+    private EditText txtIP;
+    private Button btnConnect;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_device_manager);
+
+        //hiding action bar
+        getSupportActionBar().hide();
+
+        //get edittext
+        txtIP = findViewById(R.id.device_manager_edittext_IP);
+
+        //get button
+        btnConnect = findViewById(R.id.device_manager_button_connect);
+        btnConnect.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.device_manager_button_connect) { //getting ip address from search field
+            String ip = txtIP.getText().toString();
+
+            //checking for valid ip address
+            boolean valid = isValid(ip);
+            if (valid) {
+                try {
+                    //sendPingRequest(ip, v);
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            else {
+                Toast.makeText(this, "Please enter a valid IP address.", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    private boolean isValid(String ip) {
+        if (ip.length() < 7 || ip.length() > 15)
+            return false;
+
+        String zeroTo255
+                = "(\\d{1,2}|(0|1)\\"
+                + "d{2}|2[0-4]\\d|25[0-5])";
+        String regex
+                = zeroTo255 + "\\."
+                + zeroTo255 + "\\."
+                + zeroTo255 + "\\."
+                + zeroTo255;
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(ip);
+
+        return matcher.matches();
+    }
+
+    /*private boolean sendPingRequest(String ip, View v) throws UnknownHostException, IOException {
+        InetAddress addr = InetAddress.getByName(ip);
+        Toast.makeText(this, "Connecting...", Toast.LENGTH_SHORT).show();
+        if (addr.isReachable(5000)) {
+            Toast.makeText(this, "Connected!", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        else {
+            Toast.makeText(this, "Unable to connect. Try Again.", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+    }*/
+
+    private void sendHTTP() {
+
+    }
 }
--- app/src/main/java/com/example/rad/MainActivity.java
+++ app/src/main/java/com/example/rad/MainActivity.java
@@ -2,11 +2,17 @@ package com.example.rad;
 
 import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button btnStart;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
@@ -21,5 +27,15 @@ public class MainActivity extends AppCompatActivity {
         animDraw.setEnterFadeDuration(2500);
         animDraw.setExitFadeDuration(4000);
         animDraw.start();
+
+        //get button
+        btnStart = findViewById(R.id.main_button_start);
+        btnStart.setOnClickListener(this);
+    }
+
+    public void onClick(View v) {
+        if (v.getId() == R.id.main_button_start) {
+            startActivity(new Intent(this, DeviceManagerActivity.class));
+        }
     }
 }
