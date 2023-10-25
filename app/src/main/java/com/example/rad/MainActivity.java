a7205da4220b1a884293b8f0cc6b70bce835da3f
William Hua <WilliamHua2@my.unt.edu>
2023-10-25 00:06:25 -0400

    Initial commit

---
+++ app/src/main/java/com/example/rad/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.rad;
+
+import android.graphics.drawable.AnimationDrawable;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+public class MainActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //creating dynamic background
+        ConstraintLayout constLayout = findViewById(R.id.main_layout);
+
+        AnimationDrawable animDraw = (AnimationDrawable) constLayout.getBackground();
+        animDraw.setEnterFadeDuration(2500);
+        animDraw.setExitFadeDuration(4000);
+        animDraw.start();
+    }
+}
