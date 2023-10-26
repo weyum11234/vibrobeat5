16d4a1a8485214c0ceb952aad3cb4ab4ca49ad79
khidirahmed <kka6822@nyu.edu>
2023-10-26 14:53:50 -0400

    added SongResultActivity

---
+++ app/src/main/java/com/example/rad/SongResultActivity.java
@@ -0,0 +1,21 @@
+package com.example.rad;
+
+import android.view.View;
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SongResultActivity extends AppCompatActivity implements View.OnClickListener {
+    @Override
+    public void onClick(View v) {
+
+    }
+//When search button is clicked
+    public void onCreate(Bundle sis){
+        super.onCreate(sis);
+        setContentView(R.layout.song_result_activity);
+
+
+
+    }
+    //create 3 boxes
+}
