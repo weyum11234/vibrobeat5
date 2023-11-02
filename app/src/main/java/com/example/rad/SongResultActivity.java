93bc8abde8537fb93df4d156e00e4c2ee6609e7d
William Hua <WilliamHua2@my.unt.edu>
2023-11-02 09:21:05 -0400

    fixing git

--- app/src/main/java/com/example/rad/SongResultActivity.java
+++ app/src/main/java/com/example/rad/SongResultActivity.java
@@ -1,5 +1,6 @@
 package com.example.rad;
 
+import android.content.Intent;
 import android.view.View;
 import android.os.Bundle;
 import android.widget.Button;
@@ -16,12 +17,29 @@ public class SongResultActivity extends AppCompatActivity implements View.OnClic
         super.onCreate(sis);
         setContentView(R.layout.song_result_activity);
 
+        //hiding action bar
+        getSupportActionBar().hide();
 
+        //get edittext
+        txtSong = findViewById(R.id.song_result_edittext_search);
 
+        //get button
+        btnSearch = findViewById(R.id.song_result_button_submit);
+        btnSearch.setOnClickListener(this);
+
+        btnSwitch = findViewById(R.id.song_result_button_switch);
+        btnSwitch.setOnClickListener(this);
     }
 
     public void onClick(View v) {
+        if (v.getId() == R.id.song_result_button_submit) {
+            String songTitle = txtSong.getText().toString();
 
+            //TODO code for song search
+        }
+        else if (v.getId() == R.id.song_result_button_switch) {
+            startActivity(new Intent(this, DeviceManagerActivity.class));
+        }
     }
 
 
