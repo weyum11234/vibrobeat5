55e4566000a059a1f92dc4df382eb0d71be73869
William Hua <WilliamHua2@my.unt.edu>
2023-11-08 12:10:22 -0500

    added webscraping api functionality

--- app/src/main/java/com/example/rad/SongResultActivity.java
+++ app/src/main/java/com/example/rad/SongResultActivity.java
@@ -1,13 +1,19 @@
 package com.example.rad;
 
 import android.content.Intent;
+import android.util.Log;
 import android.view.View;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Toast;
+import java.util.Scanner;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.InputStreamReader;
+import java.net.*;
 
 public class SongResultActivity extends AppCompatActivity implements View.OnClickListener {
     private EditText txtSong;
@@ -34,6 +40,34 @@ public class SongResultActivity extends AppCompatActivity implements View.OnClic
     public void onClick(View v) {
         if (v.getId() == R.id.song_result_button_submit) {
             String song = txtSong.getText().toString();
+
+            try {
+                URL url = new URL("http://10.18.194.108:5000/getbpm?song=" + song);
+                HttpURLConnection con = (HttpURLConnection) url.openConnection();
+                con.setRequestMethod("GET");
+                con.connect();
+
+                int status = con.getResponseCode();
+                if (status != 200) {
+                    Toast.makeText(this, "Could not fetch results. Please try again.", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    String inline = "";
+                    Scanner scanner = new Scanner(url.openStream());
+
+                    while (scanner.hasNext()) {
+                        inline += scanner.nextLine();
+                    }
+                    scanner.close();
+
+                    int bpm = Integer.parseInt(inline);
+
+                    //TODO successfully retrieves bpm, need to now send to ESP8266
+                }
+            }
+            catch (Exception e) {
+                Toast.makeText(this, "Something went wrong.", Toast.LENGTH_SHORT).show();
+            }
         }
         else if (v.getId() == R.id.song_result_button_switch) {
             startActivity(new Intent(this, DeviceManagerActivity.class));
